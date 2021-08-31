ddd824e9f28cd8393b725d887455aac420e998ab
sergo <nakato1995@gmail.com>
2021-08-31 09:10:41 +0400

    home commit

--- src/Components/Dashboard/Dashboard.tsx
+++ src/Components/Dashboard/Dashboard.tsx
@@ -1,18 +1,26 @@
 import React from "react"
 
-import { useParams, useLocation } from "react-router-dom"
-import { useSelector, useDispatch } from 'react-redux'
+import {useParams, useLocation} from "react-router-dom"
+import {useSelector, useDispatch} from 'react-redux'
 
-import { database } from "../../firebase"
+import {database} from "../../firebase"
 
-import {addFile, addFolder} from "../../features/fileSlice/fileSlice";
+import {addFile, removeFile, setFileStatus} from "../../features/fileSlice/fileSlice";
 
-import { doc, setDoc } from "firebase/firestore";
+import {doc, setDoc} from "firebase/firestore";
 import {db} from "../../firebase";
 import {storage} from "../../firebase";
+import {RootState} from "../../app/store";
 
+import {Link} from "react-router-dom"
 
-
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import Checkbox from "@material-ui/core/Checkbox";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 
 export default function Dashboard() {
@@ -20,24 +28,26 @@ export default function Dashboard() {
     // const { state = {} } = useLocation()
     // const { folder, childFolders, childFiles } = useFolder(folderId, state.folder)
 
-    const ROOT_FOLDER = { name: "Root", id: null, path: [] }
+    const ROOT_FOLDER = {name: "Root", id: null, path: []}
 
     const [open, setOpen] = React.useState<boolean>(false);
     const [name, setName] = React.useState<string>("");
 
 
-
     // const file = useSelector((state: any) => state.file.folder)
-    const { userEmail } = useSelector((state:any) => state.user);
-    const { folder } = useSelector((state:any) => state.files);
+    const {userEmail} = useSelector((state: any) => state.user);
+    const todoList = useSelector((state: RootState) => state);
+
+    // console.log(fileList)
+
+    // const { folder } = useSelector((state:any) => state.files);
 
     let currentdate = new Date();
 
 
-
     console.log(userEmail)
 
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
 
     function closeModal() {
         setOpen(false)
@@ -48,66 +58,66 @@ export default function Dashboard() {
     }
 
 
-
-    const handleSubmit = async (e:any) => {
+    const handleSubmit = async (e: any) => {
         e.preventDefault()
-        dispatch(addFolder({
-            title: name
-        }))
+        dispatch(addFile(name))
         closeModal()
     }
 
     return (
         <>
             <div>
-                <div className="d-flex align-items-center">
-                    {/*<FolderBreadcrumbs currentFolder={folder} />*/}
-                    {/*<AddFileButton currentFolder={folder} />*/}
-                    {/*<AddFolderButton currentFolder={folder} />*/}
 
-
-                    <h1>duuuudeeeeeeeee</h1>
-                </div>
-                {/*{childFolders.length > 0 && (*/}
-                {/*    <div className="d-flex flex-wrap">*/}
-                {/*        {childFolders.map(folder => (*/}
-                {/*            <div*/}
-                {/*                key={folder.id}*/}
-                {/*                style={{ maxWidth: "250px" }}*/}
-                {/*                className="p-2"*/}
-                {/*            >*/}
-                {/*                <Folder folder={folder} />*/}
-                {/*            </div>*/}
-                {/*        ))}*/}
-                {/*    </div>*/}
-                {/*)}*/}
-                {/*{childFolders.length > 0 && childFiles.length > 0 && <hr />}*/}
-                {/*{childFiles.length > 0 && (*/}
-                {/*    <div className="d-flex flex-wrap">*/}
-                {/*        {childFiles.map(file => (*/}
-                {/*            <div key={file.id} style={{ maxWidth: "250px" }} className="p-2">*/}
-                {/*                <File file={file} />*/}
-                {/*            </div>*/}
-                {/*        ))}*/}
-                {/*    </div>*/}
-                {/*)}*/}
                 <form onSubmit={handleSubmit}>
-                        <form>
-                            <label>Folder Name</label>
-                            <input
-                                type="text"
-                                required
-                                value={name}
-                                onChange={e => setName(e.target.value)}
-                            />
-                        </form>
-                        <button  onClick={closeModal}>
-                            Close
-                        </button>
-                        <button  type="submit">
-                            Add Folder
-                        </button>
+                    <form>
+                        <label>Folder Name</label>
+                        <input
+                            type="text"
+                            required
+                            value={name}
+                            onChange={e => setName(e.target.value)}
+                        />
+                    </form>
+                    <button type="submit">
+                        Add Folder
+                    </button>
                 </form>
+
+                <List>
+                    {todoList.files.map((file: any) => (
+                        <Link to={`/folder/${file.id}`}>
+                            <ListItem key={file.id}>
+                                <ListItemText
+                                    style={{
+                                        textDecoration: file.completed ? "line-through" : "none",
+                                    }}
+                                >
+                                    {file.description}
+                                </ListItemText>
+                                <ListItemSecondaryAction>
+                                    <IconButton
+                                        onClick={() => {
+                                            dispatch(removeFile(file.id));
+                                        }}
+                                    >
+                                        <DeleteIcon/>
+                                    </IconButton>
+                                    <Checkbox
+                                        edge="end"
+                                        value={file.completed}
+                                        onChange={() => {
+                                            dispatch(
+                                                setFileStatus({completed: !file.completed, id: file.id})
+                                            );
+                                        }}
+                                    />
+                                </ListItemSecondaryAction>
+                            </ListItem>
+                        </Link>
+
+                    ))}
+
+                </List>
             </div>
         </>
     )
--- src/features/fileSlice/fileSlice.ts
+++ src/features/fileSlice/fileSlice.ts
@@ -1,35 +1,101 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 
-interface FileState {
-    folderId: number | string;
-    folder: any;
-    childFolders: any[];
-    childFiles: any[];
+
+interface File {
+    id: number | string;
+    description: string;
+    completed: boolean;
 }
 
-const initialState = {
-    folder: [],
+const initialState = [] as File[];
 
-} as FileState ;
+
 
 const fileSlice = createSlice({
-    name: "file",
+    name: "files",
     initialState,
     reducers: {
-        addFolder: (state, action) => {
-            const newTodo = {
-                id: Date.now(),
-                title: action.payload.title,
-            };
-            state.folder.push(newTodo);
-            // state.folder = action.payload.folder;
+        addFile: {
+            reducer: (state, action: PayloadAction<File>) => {
+                state.push(action.payload);
+            },
+            prepare: (description: string) => ({
+                payload: {
+                    id: Date.now(),
+                    description,
+                    completed: false,
+                } as File,
+            }),
         },
-
-        addFile: (state, action) => {
-
-        }
-    }
+        removeFile(state, action: PayloadAction<string>) {
+            const index = state.findIndex((file) => file.id === action.payload);
+            state.splice(index, 1);
+        },
+        //update todos
+        updateFile: (state, action) => {
+            return state.map((file) => {
+                if (file.id === action.payload.id) {
+                    return {
+                        ...file,
+                        item: action.payload.item,
+                    };
+                }
+                return file;
+            });
+        },
+        setFileStatus(
+            state,
+            action: PayloadAction<{ completed: boolean; id: string }>
+        ) {
+            const index = state.findIndex((file) => file.id === action.payload.id);
+            state[index].completed = action.payload.completed;
+        },
+    },
 });
 
-export const {addFolder, addFile} = fileSlice.actions;
+export const { addFile, removeFile, setFileStatus } = fileSlice.actions;
 export default fileSlice.reducer;
+
+
+
+
+
+
+//
+// interface FileState {
+//     folderId: number | string;
+//     folder: any;
+//     childFolders: any[];
+//     childFiles: any[];
+// }
+//
+// const initialState =[] as FileState[] ;
+//
+// const fileSlice = createSlice({
+//     name: "file",
+//     initialState,
+//     reducers: {
+//         addFolder:{
+//             reducer: (state:any, action:PayloadAction<FileState>) => {
+//                 state.push(action.payload);
+//             },
+//
+//             prepare: (description: string) => ({
+//                 payload: {
+//                     id: uuidv4(),
+//                     description,
+//                     completed: false,
+//                 } as Todo,
+//             }),
+//         } ,
+//
+//
+//
+//         addFile: (state, action) => {
+//
+//         }
+//     }
+// });
+//
+// export const {addFolder, addFile} = fileSlice.actions;
+// export default fileSlice.reducer;
